865f954b1acb6bd7aa7623bb991cd888850526ca
SRINIVASAN <srini172003@gmail.com>
2025-04-03 15:45:39 +0530

    day2

---
+++ dataTypes.java
@@ -0,0 +1,24 @@
+package day;
+
+public class dataTypes {
+		public static void main(String[] args) {
+			byte age = 55;
+			short kilometers = 2569;
+			int number = 5698;
+			float cost = 265.4f;
+			long address = 5465168766l;
+			char initial = 'S';
+			boolean choose = true;
+			
+			
+			
+			System.out.println("byte"+age);
+			System.out.println("short"+kilometers);
+			System.out.println("byte"+number);
+			System.out.println("byte"+cost);
+			System.out.println("long"+address);
+			System.out.println("char"+ "   " +initial);
+			System.out.println("boolean"+"    "+choose);
+		}
+
+}
